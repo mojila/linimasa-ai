b9d5c89f8074f7fe6a99d858498e55625a83c930
mojila <me.mojila@gmail.com>
2025-07-06 20:19:42 +0700

    remove frontend

--- web/app/chat/[roomId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatContainer } from "@/components/chat-container"
-
-export default function RoomPage({ params }: { params: { roomId: string } }) {
-  return <ChatContainer roomId={params.roomId} />
-}
